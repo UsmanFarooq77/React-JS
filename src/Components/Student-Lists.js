343e13b64974ebcd0da24a14ecb8928403a04195
UsmanFarooq77 <usmanfarooq7721@gmail.com>
2021-08-27 23:07:20 +0500

    Implement the parent to child and child to parent values pass.

--- src/Components/Student-Lists.js
+++ src/Components/Student-Lists.js
@@ -1,14 +1,34 @@
-import {React} from 'react';
+import { React } from 'react';
 
-const StudentList = (props) => {
+// const StudentList = ({ parent, onchange }) => {
+
+    const StudentList = (props) => {
+    // This method is used in Class Components.
+    // onTrigger = (event) => {
+    //     this.props.parentCallback("Data from child");
+    //     event.preventDefault();
+    // }
+
+    // This method is used in functional Components.
+    const onTrigger = (event) => {
+        props.onchange(event.target.value);
+        // event.preventDefault();
+    }
+
     return (
-    <>
-    
-        <h1>{props.parent}</h1>
-        <p>Hi, i am your child Student Lists.</p>
-        {/* <p>{props.parent = "Child here"}</p> */}
-    
-    </>
+        <>
+            <h1>{props.parent}</h1>
+            {/* <h1>{parent}</h1> */}
+            <p>Hi, i am your child Student Lists.</p>
+            <p></p>
+            {/* <p>{props.parent = "Child here"}</p> */}
+            <div>
+                <form>
+                    {/* <input onChange={(event) => onchange(event.target.value)} /> */}
+                    <input onChange={onTrigger}/>
+                </form>
+            </div>
+        </>
     )
 }
 export default StudentList;
--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,17 +1,28 @@
-import {React} from 'react';
+import { render } from '@testing-library/react';
+import React, { useState, Component } from 'react';
 import { Link } from 'react-router-dom';
 import StudentList from './Student-Lists';
-
 const Student = () => {
+    // This way of method is used in Class Components.
+    // handleCallback = (childData) => {
+    //     this.setState({ data: childData })
+    // }
+    const [value, setValue] = useState('goos')
+    // render() {
+    const onchange = (dataFromChild) => {
+        setValue(dataFromChild);
+        // console.log('Data from child is = ', dataFromChild);
+    }
     return (
-    <>
-        
-            <StudentList parent="Hi i am Parent of all Student Lists."/>
+        <>
+            {/* This way is used in Class componets. */}
+            {/* <StudentList parent="Hi i am Parent of all Student Lists." parentCallback={this.handleCallback} /> */}
+            <StudentList parent="Hi i am Parent of all Student Lists." onchange={(e) => { onchange(e) }} />
             <p>Hi i am Student.</p>
             <Link to="/student-list">Student List</Link>
-        
-    </>
+            <p>{value}</p>
+        </>
     )
+    // }
 }
-
 export default Student;
